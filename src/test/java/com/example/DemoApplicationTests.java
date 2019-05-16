d5a805eae32950b382cd1c454e539cacbe949a04
long-manith <50666723+long-manith@users.noreply.github.com>
2019-05-16 11:02:51 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -24,29 +24,7 @@ public class DemoApplicationTests {
     public void shouldAnswerWithTrue()
     {
         BasicConfigurator.configure();
-        logger.debug("Hello toto*****************************************************************************************************************************************************************************************************");
+        logger.debug("Hello toto**************************************************************");
         assertTrue( true );
     }
-    public Integer additionner (Integer a, Integer b)
-  {
- 		Integer somme;
- 		somme = a - b;
-		System.out.println("a = " + a + ", b = " + b);
-		System.out.println("La somme vaut " + somme);
-                return somme;
-   }
-      @Test
-    public void additionner_integers_should_return_an_integer () {
-                 Integer somme = additionner(2, 3) ;
-                 if (somme == 5)     
-                 { 
-                 System.out.println("La somme vaut " + somme + " OK");
-                 assertTrue( true );
-                 }
-                 else
-                 {
-                 System.out.println("La somme vaut " + somme + " KO");
-                 assertTrue( false);
-                 }
-  }
 }
