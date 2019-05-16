841e3faf8fad70d700df9f8ccbeb31addee52b9f
long-manith <long-manith@gmail.com>
2019-05-16 10:26:38 +0200

    ajout de modif

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -12,5 +12,33 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+    public void shouldAnswerWithTrue()
+    {
+        PropertyConfigurator.configure("log4j.properties");
+        BasicConfigurator.configure();
+        logger.debug("Hello toto*****************************************************************************************************************************************************************************************************");
+        assertTrue( true );
+    }
+    public Integer additionner (Integer a, Integer b)
+  {
+ 		Integer somme;
+ 		somme = a + b;
+		System.out.println("a = " + a + ", b = " + b);
+		System.out.println("La somme vaut " + somme);
+                return somme;
+   }
+      @Test
+    public void additionner_integers_should_return_an_integer () {
+                 Integer somme = additionner(2, 3) ;
+                 if (somme == 5)     
+                 { 
+                 System.out.println("La somme vaut " + somme + " OK");
+                 assertTrue( true );
+                 }
+                 else
+                 {
+                 System.out.println("La somme vaut " + somme + " KO");
+                 assertTrue( false);
+                 }
 
 }
