e2b9837d0843a4c563ec6bf9f1b6faa1f299b7a8
long-manith <50666723+long-manith@users.noreply.github.com>
2019-05-16 15:19:54 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -24,6 +24,7 @@ public class DemoApplicationTests {
 	@Test
     public void shouldAnswerWithTrue()
     {
+	BasicConfigurator.configure();
         BasicConfigurator.configure();
         logger.debug("Hello toto*****************************************************************************************************************************************************************************************************");
         logger.debug("Hello toto**************************************************************");
